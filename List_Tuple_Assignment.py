3b77ee8264acbe658ea962d5fcea36a946fda1e4
280073-tejas <Tejas.ChakradharraoChaukhande@ust.com>
2025-07-24 18:18:03 +0530

    Update List_Tuple_Assignment.py

--- List_Tuple_Assignment.py
+++ List_Tuple_Assignment.py
@@ -1,13 +1,22 @@
 '''
-Q1) Print Even and Odd number from list.
-Q2) Create list called "even", store all the even numbers, in the range of 1 to 20.
-Q3) Create list called "odd", store all the odd numbers, int the range of 1,20.
-Q4) Take "even" and "odd" list , merge it a list called "numbers" and sort it.
-Q5) Create a nested list called "Students", each index store the student information.
-Q6) WAP to check list contains a  pallindrome of element.
-      ex..   [1,2,3,2,1]  ----> Pallindrome
-               ['a','b','c','d','b','c','a']  ----> Not Pallindrome
-Q7) WAP to count the number of students with "A" grade  in the following tuple.
-     grade = ("C","D","A","A","B","B","A")
-Q8) WAP to convert above tuple into list and sort them.
+Q1) Create list called "even", store all the even numbers, in the range of 1 to 20.
+Q2) Create list called "odd", store all the odd numbers, int the range of 1,20.
+Q3) Take "even" and "odd" list  from previous solution, merge it in new list called "numbers" and sort it.
+Q4) Create a nested list for called "Students" for 5 student, each index store the student information. ex.. ["name",roll,marks].
+Q5) Write a Python program to find the second largest number in a list.
+ 
+Q6) WAP to print unique element from list.
+     ex... nums = [4,3,5,6,3,4,6]      (o/p:- 5 is unique from list)
+ 
+Q7) Given a tuple of numbers, find the max and min elements.
+  ex.. tup = (11,26,45,23,15,18)
+ 
+Q8) Retrieve the 'G' from following list using positive indexing
+       L1 = [1, 2, 'hi', (21, 78, [-2, -4, ('Bahubali', 'KGF', 'RRR')])]
+ 
+Q9) WAP to retrieve the 'Sweet' string from the following nested list using Positive indexing
+       L2 = [21, ['Anil', 'Education', [['Java', 'Kova'], ['Programming', 'Sugar', 'Sweet', 'Wheat']]], 7065, 5, 2034, [1, 2]]
+ 
+Q10) WAP to extract 'Bengaluru' in reverse order using negative indexing from following string
+      s2 = 'Hello I am going to Bengaluru How are you doing?'
 '''
