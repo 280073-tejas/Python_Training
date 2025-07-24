69143bdd0becb6961d54a0d20a89635f7c47431c
280073-tejas <Tejas.ChakradharraoChaukhande@ust.com>
2025-07-24 13:01:30 +0530

    Added assignment on the list and tuples

---
+++ List_Tuple_Assignment.py
@@ -0,0 +1,13 @@
+'''
+Q1) Print Even and Odd number from list.
+Q2) Create list called "even", store all the even numbers, in the range of 1 to 20.
+Q3) Create list called "odd", store all the odd numbers, int the range of 1,20.
+Q4) Take "even" and "odd" list , merge it a list called "numbers" and sort it.
+Q5) Create a nested list called "Students", each index store the student information.
+Q6) WAP to check list contains a  pallindrome of element.
+      ex..   [1,2,3,2,1]  ----> Pallindrome
+               ['a','b','c','d','b','c','a']  ----> Not Pallindrome
+Q7) WAP to count the number of students with "A" grade  in the following tuple.
+     grade = ("C","D","A","A","B","B","A")
+Q8) WAP to convert above tuple into list and sort them.
+'''
